38bf7d277530732031699e8fd94d277f4fb2e807
gabrielfmendesm <gabrielfmm@al.insper.edu.br>
2024-09-16 12:29:31 -0300

    Projeto

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM amazoncorretto:21
-COPY target/Simulado-0.0.1-SNAPSHOT.jar /app.jar
+COPY target/Prova-0.0.1-SNAPSHOT.jar /app.jar
 ENTRYPOINT ["java", "-jar", "/app.jar"]
--- src/main/java/br/Insper/Simulado/Jogadores/JogadorRepository.java
+++
@@ -1,9 +0,0 @@
-package br.Insper.Simulado.Jogadores;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.List;
-
-public interface JogadorRepository extends MongoRepository<Jogador, String> {
-    List<Jogador> findByNome(String nome);
-}
