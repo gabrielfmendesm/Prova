17e77e83f71a51f3a4bcb4a34efe65c2ed8f4f9a
gabrielfmendesm <gabrielfmm@al.insper.edu.br>
2024-09-16 12:59:39 -0300

    Reset

--- src/main/java/br/Insper/Prova/Jogadores/JogadorRepository.java
+++
@@ -1,9 +0,0 @@
-package br.Insper.Prova.Jogadores;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.List;
-
-public interface JogadorRepository extends MongoRepository<Jogador, String> {
-    List<Jogador> findByNome(String nome);
-}
